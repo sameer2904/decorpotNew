bc7a864019fc528de37f457c25be0a36c86c9c66
Sameer Saurav <sameersaurav2904@gmail.com>
2016-05-30 12:28:10 +0530

    packages api completed

--- src/main/java/com/decorpot/controller/ApartmentController.java
+++ src/main/java/com/decorpot/controller/ApartmentController.java
@@ -1,5 +1,6 @@
 package com.decorpot.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.decorpot.rest.model.ApartmentConfigs;
+import com.decorpot.rest.model.Packages;
 import com.decorpot.services.ApartmentService;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 
 @RestController
 @RequestMapping(value = "/apartments")
@@ -17,14 +21,25 @@ public class ApartmentController {
 
 	@Autowired
 	private ApartmentService apartmentService;
-	
+
 	@RequestMapping(method = RequestMethod.GET)
 	public List<ApartmentConfigs> getAllApartments() {
 		return apartmentService.getAllApartments();
 	}
-	
+
 	@RequestMapping(value = "/{apartmentName}", method = RequestMethod.GET)
 	public ApartmentConfigs getAllFloorPlanByApartmentName(@PathVariable("apartmentName") String apartmentName) {
 		return apartmentService.getAllFloorPlanByApartmentName(apartmentName);
+	}
+
+	@RequestMapping(value = "/{apartmentType}/{floodPlanId}", method = RequestMethod.GET)
+	public List<Packages> getAllPackagesByFloorplanId(@PathVariable("apartmentType") String apartmentType,
+			@PathVariable("floodPlanId") int floodPlanId) throws JsonParseException, JsonMappingException, IOException {
+
+		if(apartmentType.equals("3BHK")) {
+			return apartmentService.get3BHKApartmentByFloorplanId(floodPlanId);
+		}else{
+			return apartmentService.get2BHKApartmentByFloorplanId(floodPlanId);
+		}
 	}
 }
