a54635ace9acdb011e7f927f037ce2ed13a6340b
govardhan <govardhan.s92@gmail.com>
2016-06-19 18:54:39 +0530

    test

--- src/main/webapp/resources/core/js/application/filters.js
+++ src/main/webapp/resources/core/js/application/filters.js
@@ -33,14 +33,33 @@ decortFilters.filter('capitalize', function() {
         return (!!input) ? input.replace(reg, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}) : '';
       }
     });
-decortFilters.filter("displayprice", function() {
-  return function(input,comma) {
-    if (isNaN(parseFloat(input))) return "";
-    comma = (typeof comma==='undefined') ? "." : ",";
-    input = Math.round(parseFloat(input)*100)/100;
-    var d = input.toString().split(".");
-    if (d.length===1) return input+comma+"-";
-    if (d[1].length<2) return input+"0";
-    return input;
-  }
+decortFilters.filter("formatPrice", function() {
+  return function(price, digits, thoSeperator, decSeperator, bdisplayprice) {
+    var i;
+    digits = (typeof digits === "undefined") ? 2 : digits;
+    bdisplayprice = (typeof bdisplayprice === "undefined") ? true : bdisplayprice;
+    thoSeperator = (typeof thoSeperator === "undefined") ? "." : thoSeperator;
+    decSeperator = (typeof decSeperator === "undefined") ? "," : decSeperator;
+    price = price.toString();
+    var _temp = price.split(".");
+    var dig = (typeof _temp[1] === "undefined") ? "00" : _temp[1];
+    if (bdisplayprice && parseInt(dig,10)===0) {
+        dig = "-";
+    } else {
+        dig = dig.toString();
+        if (dig.length > digits) {
+            dig = (Math.round(parseFloat("0." + dig) * Math.pow(10, digits))).toString();
+        }
+        for (i = dig.length; i < digits; i++) {
+            dig += "0";
+        }
+    }
+    var num = _temp[0];
+    var s = "",
+        ii = 0;
+    for (i = num.length - 1; i > -1; i--) {
+        s = ((ii++ % 3 === 2) ? ((i > 0) ? thoSeperator : "") : "") + num.substr(i, 1) + s;
+    }
+    return s + decSeperator + dig;
+}
 })
